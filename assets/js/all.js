0c3a4f9bcff8e69295de860ad5a6ea2495c83b3d
iiujung <foradadad@gmail.com>
2021-09-24 14:24:27 +0800

    Update 2021-09-24T06:24:22.929Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,38 @@
 "use strict";
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+}); // scroll to top
+
+var topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 //# sourceMappingURL=all.js.map
